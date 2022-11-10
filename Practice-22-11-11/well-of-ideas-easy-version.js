c1831c278d0cfdc39bbec3ff54dc7938b7eb7184
Cy <aryssesanvicente@yahoo.com>
2022-11-11 00:48:47 +0800

    Coding challenge for 11-11

---
+++ Practice-22-11-11/well-of-ideas-easy-version.js
@@ -0,0 +1,21 @@
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+    let count = 0
+    x.forEach(function (x) {
+        if (x == 'good') {
+            count ++
+        } else {
+            return count
+        }
+    })
+    if (count > 0 && count < 3) {
+        return 'Publish!'
+    } else if (count > 2) {
+        return 'I smell a series!'
+    } else {
+        return 'Fail!'
+    }
+}
