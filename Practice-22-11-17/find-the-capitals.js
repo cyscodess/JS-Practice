d51444bacde37a4346c4efe73f6d9c04ed387ab0
Cy <aryssesanvicente@yahoo.com>
2022-11-17 22:53:49 +0800

    Coding challenge for 11-17

---
+++ Practice-22-11-17/find-the-capitals.js
@@ -0,0 +1,10 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+var capitals = function (word) {
+	let newArr = word.split("").filter(letter => letter === letter.toUpperCase())
+    return newArr.map(letter => word.indexOf(letter))
+}
