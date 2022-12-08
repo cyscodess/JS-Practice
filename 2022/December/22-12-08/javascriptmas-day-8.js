8e02d75d0dc76aa42a08a3cd50716faed1a01559
Cy <aryssesanvicente@yahoo.com>
2022-12-08 14:47:27 +0800

    Coding challenge for 12-08

---
+++ 2022/December/22-12-08/javascriptmas-day-8.js
@@ -0,0 +1,51 @@
+function validTime(str) {
+    return parseInt(str.split(":")[0]) <= 24 && parseInt(str.split(":")[1]) <= 60
+}
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
+
