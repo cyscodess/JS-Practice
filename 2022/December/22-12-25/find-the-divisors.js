0b73ae4e1624bc67a2da08851b00faee0aef5783
Cy <aryssesanvicente@yahoo.com>
2022-12-25 02:39:51 +0800

    Coding challenge for 12-25

---
+++ 2022/December/22-12-25/find-the-divisors.js
@@ -0,0 +1,15 @@
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+function divisors(integer) {
+    let numbers = []
+    for (let i = 2; i < integer; i++) {
+        if (integer === i) continue
+        if (integer % i === 0) numbers.push(i)
+    }
+    return (numbers.length > 0) ? numbers : `${integer} is prime`
+};
