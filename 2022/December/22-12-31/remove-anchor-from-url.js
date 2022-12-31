943c4064235f2542cdcbe35e3022ec2396d444ac
Cy <aryssesanvicente@yahoo.com>
2022-12-31 21:44:25 +0800

    Coding challenge for 12-31

---
+++ 2022/December/22-12-31/remove-anchor-from-url.js
@@ -0,0 +1,10 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    let hashIndex = url.indexOf("#")
+    return (hashIndex > 0) ? url.slice(0, hashIndex) : url
+}
