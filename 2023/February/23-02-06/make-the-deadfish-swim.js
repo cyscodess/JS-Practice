f305288c681c860292033c98b5211e2fb2ab1638
Cy <aryssesanvicente@yahoo.com>
2023-02-06 20:10:50 +0800

    Coding challenge for 02-06

---
+++ 2023/February/23-02-06/make-the-deadfish-swim.js
@@ -0,0 +1,23 @@
+// Write a simple parser that will parse and run Deadfish.
+
+// Deadfish has 4 commands, each 1 character long:
+
+// i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+
+// parse("iiisdoso") => [ 8, 64 ]
+
+function parse( data ){
+    let count = 0
+    let arr = []
+    data.split("").forEach(letter => {
+        if (letter === "i") count += 1
+        if (letter === "d") count -= 1
+        if (letter === "s") count = count ** 2
+        if (letter === "o") arr.push(count)
+    })
+    return arr
+}
